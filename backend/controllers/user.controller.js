fe03e4bb0c8117f279bfde02e542ef5d7c282dc1
Diivvuu <divyanshubawa9@gmail.com>
2024-06-29 12:18:59 +0530

    suggested users made, working on update userprofile

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import Notification from "../models/notification.model.js";
-
+import bcrypt from "bcryptjs";
 export const getUserProfile = async (req, res) => {
   const { username } = req.params;
 
@@ -55,4 +55,70 @@ export const followUnfollowUser = async (req, res) => {
     console.log("Error in following/Unfollowing user.", error.message);
     res.status(500).json({ error: error.message });
   }
+};
+
+export const getSuggestedUsers = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const userFollowedByMe = await User.findById(userId).select("following");
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId },
+        },
+      },
+      {
+        $sample: { size: 10 },
+      },
+    ]);
+
+    const filteredUsers = users.filter(
+      (user) => !userFollowedByMe.following.includes(user._id)
+    );
+    const suggestedUsers = filteredUsers.slice(0, 4);
+
+    suggestedUsers.forEach((user) => (user.password = null));
+
+    res.status(200).json(suggestedUsers);
+  } catch (error) {
+    console.log("Error in getSuggestedUsers : ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+export const updateUser = async (req, res) => {
+  const { fullName, email, username, currentPassword, newPassword, bio, link } =
+    req.body;
+  let { profileImg, coverImg } = req.body;
+  const userId = req.user._id;
+  try {
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    if (
+      (!newPassword && currentPassword) ||
+      (!currentPassword && newPassword)
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Please provide both current and new password!" });
+    }
+    if (currentPassword && newPassword) {
+      const isMatch = await bcrypt.compare(currentPassword, user.password);
+      if (!isMatch)
+        return res.status(400).json({ error: "Current password is incorrect" });
+      if (newPassword.length < 6) {
+        return res
+          .status(400)
+          .json({ error: "Password must be at least 6 characters" });
+        //currentPassword is the one that user entered as mentnioned above we get it from req.body
+        //user.password is one which is already present in the database
+      }
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
+
+      if (profileImg) {
+      }
+      if (coverImg) {
+      }
+    }
+  } catch (error) {}
 };
