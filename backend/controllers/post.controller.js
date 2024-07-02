2cd1e72c3ca76abb330897e382f5c1f5f6300bfa
Diivvuu <divyanshubawa9@gmail.com>
2024-07-02 18:10:15 +0530

    post comment like unlike notification controller

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,119 @@
+import User from "../models/user.model.js";
+import Post from "../models/post.model.js";
+import Notification from "../models/notification.model.js";
+import { v2 as cloudinary } from "cloudinary";
+
+export const getAllPosts = async (req, res) => {};
+export const createPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    let { img } = req.body;
+    const userId = req.user._id.toString();
+
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    if (!text && !img) {
+      return res
+        .status(400)
+        .json({ error: "Post must have an image or a text" });
+    }
+
+    if (img) {
+      const uploadedResponse = await cloudinary.uploader(img);
+      img = uploadedResponse.secure_url;
+    }
+
+    const newPost = new Post({
+      user: userId,
+      text,
+      img,
+    });
+
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+    console.log("Error in createPost controller : ", error);
+  }
+};
+export const likeUnlikePost = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { id: postId } = req.params;
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    const userLikedPost = post.likes.includes(userId);
+    if (userLikedPost) {
+      //if post is already liked by user than just unlike it
+      await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+      res.status(200).json({ message: "Post unliked successfully" });
+    } else {
+      //if post is not liked by user than just like it
+      post.likes.push(userId);
+      await post.save();
+
+      const notification = new Notification({
+        from: userId,
+        to: post.user,
+        type: "like",
+      });
+      await notification.save();
+      res.status(200).json({ message: "Post liked successfully" });
+    }
+  } catch (error) {
+    console.log("Error in likeUnlikePost controller : ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+export const commentPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id;
+
+    if (!text) {
+      return res.status(400).json({ error: "Text field is required" });
+    }
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    const comment = { user: userId, text };
+    post.comments.push(comment);
+    await post.save();
+
+    res.status(200).json(post);
+  } catch (error) {
+    console.log("Error in commentOnPost controller : ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    if (post.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ error: "You are not authorized to delete this post." });
+    }
+
+    if (post.img) {
+      const imgId = post.img.split("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(imgId);
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    console.log("Error in deletePost controller : ", error);
+    res.status(500).json({ error: "Internal Server error" });
+  }
+};
---
+++ backend/routes/post.routes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  createPost,
+  likeUnlikePost,
+  commentPost,
+  deletePost,
+  getAllPosts,
+} from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+router.get("/all", protectRoute, getAllPosts);
+router.post("/create", protectRoute, createPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentPost);
+router.delete("/:id", protectRoute, deletePost);
+
+export default router;
