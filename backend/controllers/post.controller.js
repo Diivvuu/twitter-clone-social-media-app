5b23940fa178919f651880ac328414f751cff431
Diivvuu <divyanshubawa9@gmail.com>
2024-07-02 20:03:16 +0530

    get follwing, users, liked posts controllers added and tested

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -29,6 +29,24 @@ export const getAllPosts = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+export const getFollowingPosts = async (req, res) => {
+  try {
+    const userId = req.user._id; //currently authenticated user
+    const user = await User.findById(userId); //check if we have this user or not
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const following = user.following;
+
+    const feedPosts = await Post.find({ user: { $in: following } })
+      .sort({ createdAt: -1 })
+      .populate({ path: "user", select: "-password" })
+      .populate({ path: "comments.user", select: "-password" });
+    res.status(200).json(feedPosts);
+  } catch (error) {
+    console.log("Error in getFollowingPosts controller : ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 export const getLikedPosts = async (req, res) => {
   const userId = req.params.id;
   try {
@@ -45,6 +63,22 @@ export const getLikedPosts = async (req, res) => {
   } catch (error) {
     console.log("Error in getLikedPosts controller : ", error);
     res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+export const getUserPosts = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const posts = await Post.find({ user: user._id })
+      .sort({ createdAt: -1 })
+      .populate({ path: "user", select: "-password" })
+      .populate({ path: "comments.user", select: "-password" });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log("Error in getUserPosts controller : ", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 export const createPost = async (req, res) => {
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -7,12 +7,16 @@ import {
   deletePost,
   getAllPosts,
   getLikedPosts,
+  getFollowingPosts,
+  getUserPosts,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getFollowingPosts);
 router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
 router.post("/create", protectRoute, createPost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
 router.post("/comment/:id", protectRoute, commentPost);
