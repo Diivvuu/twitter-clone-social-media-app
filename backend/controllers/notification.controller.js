c0ec113e90760918ac731185d70ad8638af4f7c0
Diivvuu <divyanshubawa9@gmail.com>
2024-07-04 19:50:12 +0530

    1.done with backend

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -15,7 +15,7 @@ export const getNotification = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
-export const deleteNotification = async (req, res) => {
+export const deleteAllNotification = async (req, res) => {
   try {
     const userId = req.user._id;
 
@@ -27,3 +27,26 @@ export const deleteNotification = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+//we can add delete only one notif if we want to
+// export const deleteNotification = async (req, res) => {
+//   try {
+//     const notificationId = req.params;
+//     const userId = req.user._id;
+//     const notification = await Notification.findById(notificationId);
+
+//     if (!notification) {
+//       return res.status(404).json({ error: "Notification not found" });
+//     }
+//     if (notification.toString() !== userId.toString()) {
+//       return res
+//         .status(403)
+//         .json({ error: "You are not allowed to delete this notification" });
+//     }
+//     await Notification.findByIdAndDelete(notificationId);
+//     res.status(200).json({ message: "Notification deleted successfully" });
+//   } catch (error) {
+//     console.log("Error in deleteNotification function", error.message);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// };
--- backend/routes/notification.routes.js
+++ backend/routes/notification.routes.js
@@ -1,13 +1,17 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
 import {
-  deleteNotification,
+  deleteAllNotification,
+  // deleteNotification,
   getNotification,
 } from "../controllers/notification.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getNotification);
-router.delete("/", protectRoute, deleteNotification);
+router.delete("/", protectRoute, deleteAllNotification);
+
+//we can add deleting a single notification if we want to
+// router.delete("/:id", protectRoute, deleteNotification);
 
 export default router;
