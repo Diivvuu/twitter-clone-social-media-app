b4b0c8286d0f2a7f734390f31f3ed0a2b0b595dd
Diivvuu <divyanshubawa9@gmail.com>
2024-07-07 01:25:05 +0530

    signup and login page done, working on home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import LoginPage from "./pages/auth/login/LoginPage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex max-w-6xl">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/home/HomePage.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const HomePage = () => {
+  const [feedType, setFeedType] = useState("forYou");
+  return (
+    <>
+      <div className="flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen">
+        <div className="flex w-full border-b border-gray-700">
+          <div
+            className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+            onClick={() => setFeedType("forYou")}
+          >
+            For you
+            {feedType === "forYou" && (
+              <div className="absolute bottom-0 w-10 h-1 rounded-full bg-primary"></div>
+            )}
+            {/* to add blue line below the active header */}
+          </div>
+          <div
+            className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+            onClick={() => setFeedType("following")}
+          >
+            Following
+            {feedType === "following" && (
+              <div className="absolute bottom-0 w-10 h-1 rounded-full bg-primary"></div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
